c5fedc8a00df035aeaaf08cf71b085364a0d5c0c
Renan Benedicto Pereira <renan.pereira@fs.com.br>
2019-08-14 18:57:08 -0300

    Add new ways

---
+++ loading.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while [ true ]
+do
+   echo -ne "\b\b\\ "
+   sleep 0.2
+   echo -ne "\b\b| "
+   sleep 0.2
+   echo -ne "\b\b/ "
+   sleep 0.2
+   echo -ne "\b\b- "
+   sleep 0.2
+done
---
+++ smile-loading.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while [ true ]
+do
+   echo -ne "\b\b\b\U1F636"
+   sleep 0.2
+   echo -ne "\b\b\b\U1F610"
+   sleep 0.2
+   echo -ne "\b\b\b\U1F642"
+   sleep 0.2
+   echo -ne "\b\b\b\U1F60A"
+   sleep 0.2
+   echo -ne "\b\b\b\U1F604"
+   sleep 0.2
+   echo -ne "\b\b\b\U1F603"
+   sleep 0.2
+   echo -ne "\b\b\b\U1F603"
+   sleep 0.2
+done
