cecb62f8a9f93def49a06057b403fd9acf4f27d5
Renan Benedicto Pereira <renan.pereira@fs.com.br>
2019-08-14 18:23:08 -0300

    Adding the first version / variation of the script

---
+++ gcp-loading.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while [ true ]
+do
+   echo -ne "\b\b⠏ "
+   sleep 0.2
+   echo -ne "\b\b⠛ "
+   sleep 0.2
+   echo -ne "\b\b⠹ "
+   sleep 0.2
+   echo -ne "\b\b⠼ "
+   sleep 0.2
+   echo -ne "\b\b⠶ "
+   sleep 0.2
+   echo -ne "\b\b⠧ "
+   sleep 0.2
+done
